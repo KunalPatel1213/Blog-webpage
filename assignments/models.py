7be997e12202265470e53f316233dd785a6c0e9b
kunal patel <kunno1751@gmail.com>
2026-01-24 21:29:17 +0530

    fix the folder structure

---
+++ assignments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class About(models.Model):
+    about_heading = models.CharField(max_length=25)
+    about_description = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Abouts'   
+    def __str__(self):
+        return self.about_description
