931a200c89265aa5678af69c8077ec5dcb991d80
kunal patel <kunno1751@gmail.com>
2026-01-25 20:56:14 +0530

    fix the social links for page

--- assignments/models.py
+++ assignments/models.py
@@ -7,6 +7,18 @@ class About(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'Abouts'   
+        verbose_name_plural = 'About'   
+
+
     def __str__(self):
-        return self.about_description
+        return self.about_heading
+    
+
+class SocialLink(models.Model):
+    platform = models.CharField(max_length=25)
+    link = models.URLField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.platform
