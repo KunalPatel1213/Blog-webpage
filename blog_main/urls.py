251e47fad1fe64862a1a9958cc5c07a5ec67a1df
kunal patel <kunno1751@gmail.com>
2026-01-30 20:28:35 +0530

    fix the login button

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 
     path('blgs/search/', BlogsView.search, name='search'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
