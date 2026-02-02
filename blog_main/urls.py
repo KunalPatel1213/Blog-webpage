219139a583c6da47c31302845157d589a7c87129
kunal patel <kunno1751@gmail.com>
2026-02-02 20:23:22 +0530

    fix the bug

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -20,15 +20,19 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from blogs import views as BlogsView
+from django.contrib.auth import views as auth_views   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs' ),
+    path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
 
     path('blgs/search/', BlogsView.search, name='search'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout/')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),   
+
+    #Dashboard
+    path('dashboard/', include('dashboards.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
