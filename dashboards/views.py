9946b963bb16f4a4c78cab64ddb5f92a98288f3b
kunal patel <kunno1751@gmail.com>
2026-02-02 22:11:50 +0530

    fix the real time count category

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from blogs.models import Category, Blog
 # Create your views here.
 
 
 def dashboard(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all()
     return render(request, 'dashboard/dashboard.html')
