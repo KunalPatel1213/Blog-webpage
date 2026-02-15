c5ecd543db2fa639414910ddef9ae0637d1e031e
kunal patel <kunno1751@gmail.com>
2026-02-15 18:52:15 +0530

    fix the user in Dashboard section

--- dashboards/views.py
+++ dashboards/views.py
@@ -3,6 +3,7 @@ from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm, BlogPostForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Dashboard view
 @login_required(login_url='login')
@@ -81,10 +82,10 @@ def add_post(request):
     if request.method == 'POST':
         form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save(commit=False)  # temporarily saving the form 
+            post = form.save(commit=False)  
             post.author = request.user
-            title = form.cleaned_data['title']   # get title before saving
-            post.slug = slugify(title)  # initial slug
+            title = form.cleaned_data['title']   
+            post.slug = slugify(title)  
             post.save()
 
             # ensure uniqueness by appending ID after first save
@@ -123,3 +124,10 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users':users,
+    }
+    return render(request, 'dashboard/users.html', context)
