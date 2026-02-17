b791f3578ef62ea39016d1e014b1539dbdcd3a4d
kunal patel <kunno1751@gmail.com>
2026-02-17 16:03:21 +0530

    fix the usermodel

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -11,9 +11,14 @@ class CategoryForm(forms.ModelForm):
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = {'title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured'}
+        fields = ['title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured']
 
 class AddUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        fields = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions']
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff']
--- dashboards/views.py
+++ dashboards/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm, BlogPostForm
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
-from .forms import AddUserForm
+from .forms import AddUserForm , EditUserForm
 
 # Dashboard view
 @login_required(login_url='login')
@@ -146,3 +146,21 @@ def add_user(request):
         'form':form,
     }
     return render(request, 'dashboard/add_user.html', context)
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'form':form,
+    }
+    return render(request, 'dashboard/edit_user.html', context)
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
